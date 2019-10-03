07c9aa020c146aeb99f814f9795a12900540ea52
Andrey Maksymov <slarkumn@gmail.com>
2019-10-03 20:02:18 +0300

    implemented :D

--- Universe/Code/UI/Base/BaseViewController.swift
+++ Universe/Code/UI/Base/BaseViewController.swift
@@ -1,13 +1,9 @@
 //
-//  ViewController.swift
+//  BaseViewController.swift
 //  Universe
 //
 //  Created by Andrey Maksymov on 10/3/19.
 //  Copyright © 2019 Andrey Maksymov. All rights reserved.
 //
 
-import UIKit
-
-class ViewController: UIViewController {
-    
-}
+import Foundation
---
+++ Universe/Code/UI/Controllers/WeatherListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherListViewController.swift
+//  Universe
+//
+//  Created by Andrey Maksymov on 10/3/19.
+//  Copyright © 2019 Andrey Maksymov. All rights reserved.
+//
+
+import Foundation
